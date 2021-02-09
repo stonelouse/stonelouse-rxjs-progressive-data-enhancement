a83802f58c222e3c9cb5c983d3ffaf7bf3e8ba5a
stonelouse <c.russ@web.de>
2021-02-09 07:04:05 +0100

    progressive data enhancement

--- index.ts
+++ index.ts
@@ -1,6 +1,75 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+import { fromEvent, of, merge } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
+import { map, delay, exhaustMap, switchMap, takeUntil, tap } from 'rxjs/operators';
+
+console.clear();
+
+const searchBtn = document.getElementById("search");
+/* any: workaround for accessing 'value' property below */
+const searchInput: any = document.getElementById("searchInput");
+const pokemonName = document.getElementById("pokemonName");
+const pokemonType = document.getElementById("pokemonType");
+const evolutions = document.getElementById("evolutions");
+/* any: workaround for accessing 'src' property below */
+const pokemonImage: any = document.getElementById("pokemonImage");
+const result = document.getElementById("result");
+
+result.style.display = "none";
+
+function getPokemon(name: string) {
+  console.log('fetching the pokemon');
+  
+  /* Creates an Observable from an Ajax (fetch) request */
+  return ajax(`https://pokeapi.co/api/v2/pokemon/${name}`).pipe(
+    map(pokemon => pokemon.response),
+    delay(1500)
+  );
+}
+
+function getEvolutions(id: number) {
+  console.log('fetching the evolutions');
+ 
+  return ajax(`https://pokeapi.co/api/v2/evolution-chain/${id}`).pipe(
+    map(pokemon => pokemon.response),
+    delay(1500)
+  );
+}
+
+function addEvolutionsToPokemon(pokemon, evolutions) {
+  return {...pokemon, ...evolutions};
+}
+
+function addPokemonToUi(pokemon) {
+  console.log(pokemon);
+
+  result.style.display = 'block';
+  pokemonName.innerText = pokemon.name;
+  pokemonImage.src = pokemon.sprites.front_default;
+  pokemonType.innerText = pokemon.types[0].type.name;
+  evolutions.innerText = `Evolves to: ${pokemon?.chain?.evolves_to[0].species.name || 'loading...'}`;
+}
+
+/* fromEvent:
+   Creates an Observable that 
+   ... emits events of a specific type 
+   ... coming from the given event target. 
+
+   search for 'pikachu'
+ */
+fromEvent(searchBtn, 'click').pipe(
+  map(e=>searchInput.value),
+  exhaustMap((name) => getPokemon(name).pipe(
+    switchMap(pokemon => {
+      const pokemon$ = of(pokemon);
+      const evolution$ = getEvolutions(pokemon.id).pipe(
+        map(evolutions => {
+          return addEvolutionsToPokemon(pokemon, evolutions);
+        }));
+      return merge(pokemon$, evolution$).pipe();
+    }),
+    takeUntil(fromEvent(searchInput, 'input'))
+  )),
+).subscribe(addPokemonToUi);
